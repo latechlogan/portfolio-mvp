03d490f3358096223c6de84b3d2979cf43690635
Logan Baugh <7052181+latechlogan@users.noreply.github.com>
2025-11-17 09:41:23 -0600

    feat: add nav toggle function with js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const body = document.querySelector("body");
+const nav = document.querySelector(".nav");
+const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector(".hamburger");
+
+// nav toggle
+let navOpen = false;
+
+function toggleMenu() {
+  navOpen = !navOpen;
+
+  if (navOpen) {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  }
+
+  body.classList.toggle("nav-open");
+  navMenu.classList.toggle("active");
+  const expanded = hamburger.getAttribute("aria-expanded") === "true";
+  hamburger.setAttribute("aria-expanded", !expanded);
+}
+
+hamburger.addEventListener("click", (e) => {
+  e.preventDefault;
+  toggleMenu();
+});
+
+navMenu.addEventListener("click", (e) => {
+  e.preventDefault;
+  toggleMenu();
+});
